5b1b669f1fa7e0be1369bb3dddb81240f547ba0d
Chan Dinh <dinhhoangbaochan@gmail.com>
2024-07-09 15:23:02 +0700

    get input value for add task function

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-console.log("Hello Guys")
+const todoForm = document.getElementById('to-do-form');
+const inputTask = document.getElementById('input-task');
+
+todoForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  const taskName = inputTask.value;
+
+  console.log(inputTask.value);
+  console.log('Form is being submitted')
+})
