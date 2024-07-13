8355bad62391170665e2768846d871908063d407
Chan Dinh <dinhhoangbaochan@gmail.com>
2024-07-13 21:02:14 +0700

    add function to test edit action

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ const todoForm = document.getElementById('todo-form');
 const todoTaskInput = document.getElementById('todo-task-name');
 const todoList = document.getElementById('todo-list');
 
+let editBTN = document.querySelector('edit-btn');
+
 todoForm.addEventListener('submit', (e) => {
   e.preventDefault();
   const taskName = todoTaskInput.value;
@@ -11,7 +13,19 @@ todoForm.addEventListener('submit', (e) => {
 
 const addTask = (taskName) => {
   const todoItem = document.createElement("div");
-  todoItem.innerHTML = `<p>${taskName}</p>`
+  todoItem.innerHTML = `
+    <p>${taskName}</p>
+    <a class="edit-btn">Edit</a>
+    <a class="delete-btn">Delete</a>
+  `
   todoList.appendChild(todoItem);
 }
 
+const editAction = (e) => {
+  const currentItem = e.target;
+  const itemClassName = currentItem.className;
+  if (itemClassName === 'edit-btn')
+  console.log(e.target.parentElement)
+}
+
+document.addEventListener("click", editAction);
