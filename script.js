ca78a0930c0f0b5157fcbe3430ed7ccac2aa34b2
Chan Dinh <dinhhoangbaochan@gmail.com>
2024-07-13 20:01:23 +0700

    create add task function

--- script.js
+++ script.js
@@ -1,12 +1,17 @@
-const todoForm = document.getElementById('to-do-form');
-const inputTask = document.getElementById('input-task');
+const todoForm = document.getElementById('todo-form');
+const todoTaskInput = document.getElementById('todo-task-name');
+const todoList = document.getElementById('todo-list');
 
 todoForm.addEventListener('submit', (e) => {
   e.preventDefault();
-
-  const taskName = inputTask.value;
-
-  console.log(inputTask.value);
-  console.log('Form is being submitted')
+  const taskName = todoTaskInput.value;
+  addTask(taskName);
+  todoTaskInput.value = "";
 })
+
+const addTask = (taskName) => {
+  const todoItem = document.createElement("div");
+  todoItem.innerHTML = `<p>${taskName}</p>`
+  todoList.appendChild(todoItem);
+}
 
