ea12cec0358ef2df20f4af93defb06c239eed13f
Chan Dinh <dinhhoangbaochan@gmail.com>
2024-07-20 16:04:32 +0700

    add count items function

--- script.js
+++ script.js
@@ -26,13 +26,18 @@ const editAction = (e) => {
   const parentOfCurrent = e.target.parentElement;
   const currentItem = e.target;
   const currentItemClass = currentItem.className;
-  // const currentItemText = parentOfCurrent;
   
   if (currentItemClass === 'edit-btn') {
     let currentTaskName = parentOfCurrent.getElementsByTagName("p")[0].innerText;
 
     todoTaskInput.value = currentTaskName;
   }
+}
+
+// Count total todo items on screen
+const countItems = () => {
+  const allTodoItems = todoList.querySelectorAll('.todo-item');
+  return allTodoItems.length;
 }
 
 document.addEventListener("click", editAction);
