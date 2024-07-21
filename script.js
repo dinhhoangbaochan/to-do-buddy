7dfd59bfb7eb596d19f3c0e1848d682e944f4c48
Chan Dinh <dinhhoangbaochan@gmail.com>
2024-07-21 20:00:28 +0700

    get id of being edited item

--- script.js
+++ script.js
@@ -11,25 +11,43 @@ todoForm.addEventListener('submit', (e) => {
   todoTaskInput.value = "";
 })
 
+
+/**
+ * What do we want?
+ * When a task name is written, then submit
+ * Count the total tasks existing on screen
+ * Get the total, assign an id to being added item using that number plus one
+ * 
+ * But, we need to know, if we are adding a new one or we are editting an existing one.
+ * So, in our form, we have a hidden input field to check for the form's action
+ * Before performing action, check the action type.
+ */
 const addTask = (taskName) => {
   const todoItem = document.createElement("div");
+  const currentTotal = countItems();
+  const itemID = `item-${currentTotal + 1}`
+
   todoItem.classList.add("todo-item");
+  todoItem.id = itemID;
   todoItem.innerHTML = `
     <p>${taskName}</p>
     <a class="edit-btn">Edit</a>
     <a class="delete-btn">Delete</a>
-  `
+  `;
+
   todoList.appendChild(todoItem);
 }
 
 const editAction = (e) => {
   const parentOfCurrent = e.target.parentElement;
-  const currentItem = e.target;
-  const currentItemClass = currentItem.className;
+  const currentEditBtn = e.target;
+  const currentBtnClass = currentEditBtn.className;
   
-  if (currentItemClass === 'edit-btn') {
-    let currentTaskName = parentOfCurrent.getElementsByTagName("p")[0].innerText;
+  if (currentBtnClass === 'edit-btn') {
+    const currentTaskName = parentOfCurrent.getElementsByTagName("p")[0].innerText;
+    const currentTaskID = parentOfCurrent.id;
 
+    console.log(currentTaskID);
     todoTaskInput.value = currentTaskName;
   }
 }
